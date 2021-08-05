abee2c02e8e25670d57f41e86d19320fc89742b0
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-05 10:13:25 +0800

    SignInStatuses.cs (small)

--- Assets/Scripts/Server/PlayFab/Unattachable/SignInStatuses.cs
+++ Assets/Scripts/Server/PlayFab/Unattachable/SignInStatuses.cs
@@ -1,6 +1,6 @@
 namespace Server.General {
 	internal static class SignInStatuses: object {
-		internal enum SignInStatus: byte {
+		internal enum SignInStatus: int {
 			None,
 			NoUsernameOrEmail,
 			NoPassword,
