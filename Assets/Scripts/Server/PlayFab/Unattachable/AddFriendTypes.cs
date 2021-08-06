dbf317c7a3495571af02fabf4d4d4e5a81d3b31c
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-06 17:04:21 +0800

    AddFriendTypes.cs

---
+++ Assets/Scripts/Server/PlayFab/Unattachable/AddFriendTypes.cs
@@ -0,0 +1,11 @@
+namespace Server.PlayFab {
+	internal static class AddFriendTypes: object {
+		internal enum AddFriendType: int {
+			DisplayName,
+			Username,
+			Email,
+			PlayFabID,
+			Amt
+		}
+	}
+}
