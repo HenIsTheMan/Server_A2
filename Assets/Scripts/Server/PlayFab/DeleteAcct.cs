d2b00e2d6a078490150d9e46ffe983ba1ac1cf66
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-06 10:53:08 +0800

    DeleteAcct.cs (create)

---
+++ Assets/Scripts/Server/PlayFab/DeleteAcct.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Server.PlayFab {
+    internal sealed class DeleteAcct: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal DeleteAcct(): base() {
+        }
+
+        static DeleteAcct() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+    }
+}
