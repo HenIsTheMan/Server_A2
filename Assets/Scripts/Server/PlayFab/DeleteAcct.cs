0db0f9844df189c53185c5ce40d5c7748ee5585b
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-06 11:46:21 +0800

    DeleteAcct (almost)

--- Assets/Scripts/Server/PlayFab/DeleteAcct.cs
+++ Assets/Scripts/Server/PlayFab/DeleteAcct.cs
@@ -1,8 +1,26 @@
+using PlayFab;
+using PlayFab.ClientModels;
+using Server.General;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Server.PlayFab {
     internal sealed class DeleteAcct: MonoBehaviour {
         #region Fields
+
+        [SerializeField]
+        private TMP_Text editAcctMsgTmp;
+
+        [SerializeField]
+        private string acctDeletedText;
+
+        [SerializeField]
+        private Color acctDeletedTextColor;
+
+        [SerializeField]
+        private UnityEvent myUnityEvent;
+
         #endregion
 
         #region Properties
@@ -11,6 +29,11 @@ namespace Server.PlayFab {
         #region Ctors and Dtor
 
         internal DeleteAcct(): base() {
+            editAcctMsgTmp = null;
+            acctDeletedText = string.Empty;
+            acctDeletedTextColor = Color.white;
+
+            myUnityEvent = null;
         }
 
         static DeleteAcct() {
@@ -20,5 +43,29 @@ namespace Server.PlayFab {
 
         #region Unity User Callback Event Funcs
         #endregion
+
+        public void OnClick() {
+            PlayFabClientAPI.ExecuteCloudScript(
+                new ExecuteCloudScriptRequest() {
+                    FunctionName = "DeleteAcct",
+                    GeneratePlayStreamEvent = true,
+                },
+                OnExecuteCloudScriptSuccess,
+                OnExecuteCloudScriptFailure
+            );
+        }
+
+        private void OnExecuteCloudScriptSuccess(ExecuteCloudScriptResult result) {
+            Console.Log("ExecuteCloudScriptSuccess!");
+
+            editAcctMsgTmp.text = acctDeletedText;
+            editAcctMsgTmp.color = acctDeletedTextColor;
+
+            myUnityEvent?.Invoke();
+        }
+
+        private void OnExecuteCloudScriptFailure(PlayFabError _) {
+            Console.LogError("ExecuteCloudScriptFailure!");
+        }
     }
 }
