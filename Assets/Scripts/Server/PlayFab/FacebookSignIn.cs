b77206871287e4ba0bd05d590845abfb56cc4830
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-07 18:23:40 +0800

    FB (almost done)

--- Assets/Scripts/Server/PlayFab/FacebookSignIn.cs
+++ Assets/Scripts/Server/PlayFab/FacebookSignIn.cs
@@ -1,13 +1,43 @@
 using PlayFab;
 using PlayFab.ClientModels;
 using Server.General;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Server.PlayFab {
     internal sealed class FacebookSignIn: MonoBehaviour {
         #region Fields
 
-        public string tokenStr;
+        [SerializeField]
+        private TMP_InputField userAccessTokenInputField;
+
+        [SerializeField]
+        private TMP_Text facebookSignInMsg;
+
+        [SerializeField]
+        private EllipsesControl facebookSignInEllipsesControl;
+
+        [SerializeField]
+        private string signingInText;
+
+        [SerializeField]
+        private Color signingInColor;
+
+        [SerializeField]
+        private string signInSuccessText;
+
+        [SerializeField]
+        private Color signInSuccessColor;
+
+        [SerializeField]
+        private string signInFailureText;
+
+        [SerializeField]
+        private Color signInFailureColor;
+
+        [SerializeField]
+        private UnityEvent myUnityEvent;
 
         #endregion
 
@@ -17,6 +47,21 @@ namespace Server.PlayFab {
         #region Ctors and Dtor
 
         internal FacebookSignIn(): base() {
+            userAccessTokenInputField = null;
+
+            facebookSignInMsg = null;
+            facebookSignInEllipsesControl = null;
+
+            signingInText = string.Empty;
+            signingInColor = Color.white;
+
+            signInSuccessText = string.Empty;
+            signInSuccessColor = Color.white;
+
+            signInFailureText = string.Empty;
+            signInFailureColor = Color.white;
+
+            myUnityEvent = null;
         }
 
         static FacebookSignIn() {
@@ -25,25 +70,44 @@ namespace Server.PlayFab {
         #endregion
 
         #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            facebookSignInMsg.text = string.Empty;
+        }
+
         #endregion
 
         public void OnClick() {
             PlayFabClientAPI.LoginWithFacebook(
                 new LoginWithFacebookRequest() {
                     CreateAccount = true,
-                    AccessToken = tokenStr
+                    AccessToken = userAccessTokenInputField.text
                 },
                 OnPlayfabFacebookAuthComplete,
                 OnPlayfabFacebookAuthFailed
             );
+
+            facebookSignInEllipsesControl.enabled = true;
+            facebookSignInMsg.text = signingInText;
+            facebookSignInMsg.color = signingInColor;
         }
 
         private void OnPlayfabFacebookAuthComplete(LoginResult result) {
-            Console.Log("PlayFab Facebook Auth Complete. Session ticket: " + result.SessionTicket);
+            Console.Log("Facebook Sign In Success: " + result.SessionTicket);
+
+            facebookSignInEllipsesControl.enabled = false;
+            facebookSignInMsg.text = signInSuccessText;
+            facebookSignInMsg.color = signInSuccessColor;
+
+            myUnityEvent?.Invoke();
         }
 
         private void OnPlayfabFacebookAuthFailed(PlayFabError error) {
-            Console.Log("PlayFab Facebook Auth Failed: " + error.GenerateErrorReport());
+            Console.Log("Facebook Sign In Failure: " + error.GenerateErrorReport());
+
+            facebookSignInEllipsesControl.enabled = false;
+            facebookSignInMsg.text = signInFailureText;
+            facebookSignInMsg.color = signInFailureColor;
         }
     }
 }
