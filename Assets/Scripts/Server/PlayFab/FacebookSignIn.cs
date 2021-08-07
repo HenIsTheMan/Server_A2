bcffeb73946ad63fab0553cdaad3242d786cdd36
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-07 17:51:05 +0800

    FB stuff

---
+++ Assets/Scripts/Server/PlayFab/FacebookSignIn.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace Server.PlayFab {
+    internal sealed class FacebookSignIn: MonoBehaviour {
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal FacebookSignIn(): base() {
+        }
+
+        static FacebookSignIn() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+        #endregion
+
+        public void OnClick() {
+        }
+    }
+}
