7fdc17c30cab57adaa0229c361781d543b24c98f
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-12 22:44:41 +0800

    SentTradeRequests.cs (create)

---
+++ Assets/Scripts/Server/PlayFab/SentTradeRequests.cs
@@ -0,0 +1,74 @@
+using IWP.General;
+using PlayFab;
+using PlayFab.ClientModels;
+using Server.General;
+using UnityEngine;
+
+namespace Server.PlayFab {
+    internal sealed class SentTradeRequests: MonoBehaviour {
+        #region Fields
+
+        [SerializeField]
+        private GameObject selectionPrefab;
+
+        [SerializeField]
+        private Transform contentTransform;
+
+        [SerializeField]
+        private int amtOfSelections;
+
+        [SerializeField]
+        private ObjPool selectionPool;
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Ctors and Dtor
+
+        internal SentTradeRequests(): base() {
+            selectionPrefab = null;
+            contentTransform = null;
+
+            amtOfSelections = 0;
+            selectionPool = null;
+        }
+
+        static SentTradeRequests() {
+        }
+
+        #endregion
+
+        #region Unity User Callback Event Funcs
+
+        private void Awake() {
+            selectionPool.InitMe(
+                amtOfSelections,
+                selectionPrefab,
+                contentTransform,
+                false
+            );
+        }
+
+        #endregion
+
+        public void OnClick() {
+            PlayFabClientAPI.GetPlayerTrades(
+                new GetPlayerTradesRequest(),
+                OnGetPlayerTradesSuccess,
+                OnGetPlayerTradesFailure
+            );
+        }
+
+        private void OnGetPlayerTradesSuccess(GetPlayerTradesResponse response) {
+            Console.Log("GetPlayerTradesSuccess!");
+
+
+        }
+
+        private void OnGetPlayerTradesFailure(PlayFabError _) {
+            Console.LogError("GetPlayerTradesFailure!");
+        }
+    }
+}
