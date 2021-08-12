639a46fa9d7f882dbd27afb0b5336e576aa2fbc3
Unknown <193541T@mymail.nyp.edu.sg>
2021-08-12 09:54:43 +0800

    ClearInvButton.cs (save)

--- Assets/Scripts/Server/PlayFab/ClearInvButton.cs
+++ Assets/Scripts/Server/PlayFab/ClearInvButton.cs
@@ -1,11 +1,36 @@
 using PlayFab;
 using PlayFab.ClientModels;
 using Server.General;
+using TMPro;
 using UnityEngine;
 
 namespace Server.PlayFab {
     internal sealed class ClearInvButton: MonoBehaviour {
         #region Fields
+
+        [SerializeField]
+        private TMP_Text invPanelMsg;
+
+        [SerializeField]
+        private EllipsesControl invPanelEllipsesControl;
+
+        private string processingText;
+
+        [SerializeField]
+        private Color processingColor;
+
+        [SerializeField]
+        private string successText;
+
+        [SerializeField]
+        private Color successColor;
+
+        [SerializeField]
+        private string failureText;
+
+        [SerializeField]
+        private Color failureColor;
+
         #endregion
 
         #region Properties
@@ -14,6 +39,17 @@ namespace Server.PlayFab {
         #region Ctors and Dtor
 
         internal ClearInvButton(): base() {
+            invPanelMsg = null;
+            invPanelEllipsesControl = null;
+
+            processingText = string.Empty;
+            processingColor = Color.white;
+
+            successText = string.Empty;
+            successColor = Color.white;
+
+            failureText = string.Empty;
+            failureColor = Color.white;
         }
 
         static ClearInvButton() {
@@ -33,14 +69,48 @@ namespace Server.PlayFab {
                 OnExecuteCloudScriptSuccess,
                 OnExecuteCloudScriptFailure
             );
+
+            MyProcessingFunc();
         }
 
         private void OnExecuteCloudScriptSuccess(ExecuteCloudScriptResult _) {
             Console.Log("ExecuteCloudScriptSuccess!");
+
+            GiftTrade.globalObj.UpdateInv();
+
+            StartCoroutine(nameof(SuccessIsComing));
         }
 
         private void OnExecuteCloudScriptFailure(PlayFabError _) {
             Console.LogError("ExecuteCloudScriptFailure!");
+
+            MyFailureFunc();
+        }
+
+        private System.Collections.IEnumerator SuccessIsComing() {
+            while(GiftTrade.globalObj.IsInvUpdating) {
+                yield return null;
+            }
+
+            MySuccessFunc();
+        }
+
+        private void MyProcessingFunc() {
+            invPanelEllipsesControl.enabled = true;
+            invPanelMsg.text = processingText;
+            invPanelMsg.color = processingColor;
+        }
+
+        private void MySuccessFunc() {
+            invPanelEllipsesControl.enabled = false;
+            invPanelMsg.text = successText;
+            invPanelMsg.color = successColor;
+        }
+
+        private void MyFailureFunc() {
+            invPanelEllipsesControl.enabled = false;
+            invPanelMsg.text = failureText;
+            invPanelMsg.color = failureColor;
         }
     }
 }
